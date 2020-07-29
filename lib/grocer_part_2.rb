35e7ead58bac7f16c7771fa799cdeae5efd18578
Benjamin Arthur Looper <benlooper08@gmail.com>
2020-07-29 16:20:05 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -5,11 +5,11 @@ def apply_coupons(cart, coupons)
   cart.each do |grocery_item|
     coupons.each do |coupon|
       if coupon[:item] == grocery_item[:item]
-        cart.append {item: grocery_item[:item] + " W/COUPON", 
+        cart.append({item: grocery_item[:item] + " W/COUPON", 
                      price: coupon[:cost] / coupon[:num],
                      clearance: true,
                      count: coupon[:num]
-                    }
+                    })
       end 
     end 
   end 
