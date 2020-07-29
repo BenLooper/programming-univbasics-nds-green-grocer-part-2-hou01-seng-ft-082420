4a54e07688c7aff8a2068fd7991d16a770b78b44
Benjamin Arthur Looper <benlooper08@gmail.com>
2020-07-29 17:56:33 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -29,13 +29,8 @@ end
 
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+
+  
+
+
 end
